6102f27bd5ab87f5e6eea59311cda7a8a36dc89c
Mathis Laroche <ecomath360@gmail.com>
2024-04-09 20:36:41 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict"
+
+/** @type HTMLCanvasElement **/
+const canvas = document.getElementById("merci-robin-canvas");
+
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, 700, 500);
+
+ctx.fillStyle = "red";
+ctx.fillRect(10, 10, 50, 50)
+
+
+function loadMerci() {
+
+}
